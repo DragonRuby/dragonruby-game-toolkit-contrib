be2c15dcedf3129af0401c730c50b5913da6e601
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2020-08-02 18:47:10 +0900

    Fix brackets

--- dragon/enumerable.rb
+++ dragon/enumerable.rb
@@ -2,7 +2,7 @@ module Enumerable
   def sum(init = 0)
     result = init
     self.each do |element|
-      result += block_given? ? yield element : element
+      result += block_given? ? (yield element) : element
     end
     result
   end
