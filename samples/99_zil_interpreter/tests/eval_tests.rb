88c26c1a4aadc536ba6736a23022d50ad162e860
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2021-09-08 07:48:56 +0900

    Add eval test for forms

--- samples/99_zil_interpreter/tests/eval_tests.rb
+++ samples/99_zil_interpreter/tests/eval_tests.rb
@@ -24,3 +24,25 @@ def test_eval_string_returns_itself(args, assert)
 
   assert.equal! result, 'Bob'
 end
+
+def test_eval_form_calls_global_value_of_func_atom(args, assert)
+  zil_context = build_zil_context(args)
+  call_args = nil
+  call_context = nil
+  zil_context.globals[:FUNC] = lambda { |args, context|
+    call_args = args
+    call_context = context
+    'return value'
+  }
+
+  result = eval_zil(
+    Syntax::Form.new(:FUNC, 'Bob', 22, Syntax::Form.new),
+    zil_context
+  )
+
+  # Evaluating its arguments should be responsibility of the function
+  # So eval_zil should pass them directly
+  assert.equal! call_args, ['Bob', 22, Syntax::Form.new]
+  assert.equal! call_context, zil_context
+  assert.equal! result, 'return value'
+end
