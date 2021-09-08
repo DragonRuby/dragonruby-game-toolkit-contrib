7b0aaa78139fe63aed7f98ef2293710ac4635d18
Kevin Fischer <kevin@agileware.jp>
2021-09-08 22:57:37 +0900

    Add basic eval implementation

--- samples/99_zil_interpreter/app/eval.rb
+++ samples/99_zil_interpreter/app/eval.rb
@@ -1,3 +1,11 @@
 def eval_zil(expression, zil_context)
-  # Please implement :D
+  case expression
+  when Numeric, String, Symbol
+    expression
+  when Syntax::Form
+    func_atom = expression.elements.first
+    func_args = expression.elements.drop(1)
+    function = zil_context.globals[func_atom]
+    function.call(func_args, zil_context)
+  end
 end
