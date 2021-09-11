a7c8b8b302c031b2bccf1fca2112e42abd5a5418
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2021-09-11 21:25:39 +0900

    Extract define_for_evaled_args_helper

--- samples/99_zil_interpreter/app/builtins.rb
+++ samples/99_zil_interpreter/app/builtins.rb
@@ -1,10 +1,17 @@
 class FunctionError < StandardError; end
 
+def define_for_evaled_args(&implementation)
+  lambda { |args, context|
+    evaled_args = args.map { |arg| eval_zil arg, context }
+    implementation.call(evaled_args, context)
+  }
+end
+
 ZIL_BUILTINS = {}
 
-ZIL_BUILTINS[:LVAL] = lambda { |args, context|
-  var_atom = eval_zil args[0], context
-  raise FunctionError, "No local value for #{var_atom}" unless context.locals.key? var_atom
+ZIL_BUILTINS[:LVAL] = define_for_evaled_args { |args, context|
+  var_atom = args[0]
+  raise FunctionError, "No local value for #{var_atom.inspect}" unless context.locals.key? var_atom
 
   context.locals[var_atom]
 }
