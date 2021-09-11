389264e376477ba919047d3cf132863171d52fb2
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2021-09-11 23:24:01 +0900

    Add Parser Error handling

--- samples/99_zil_interpreter/app/zil_repl.rb
+++ samples/99_zil_interpreter/app/zil_repl.rb
@@ -6,7 +6,12 @@ ZIL_BUILTINS[:GO] = define_for_evaled_arguments { |_, context|
 ZIL_BUILTINS[:REPL] = define_for_evaled_arguments { |_, context|
   loop do
     input = Fiber.yield
-    parsed = Parser.parse_string(input)[0]
+    begin
+      parsed = Parser.parse_string(input)[0]
+    rescue RuntimeError => e
+      context.outputs << "ParserError: #{e}"
+      next
+    end
     result = eval_zil(parsed, context)
     context.outputs << "-> #{result}"
   end
--- samples/99_zil_interpreter/tests/zil_repl_tests.rb
+++ samples/99_zil_interpreter/tests/zil_repl_tests.rb
@@ -9,6 +9,17 @@ def test_repl(args, assert)
   ]
 end
 
+def test_repl_parsing_error(args, assert)
+  zil_context = build_zil_context(args)
+  repl_fiber = build_repl_fiber(zil_context)
+
+  repl_fiber.resume '<+ xxxxx'
+
+  assert.equal! zil_context.outputs, [
+    'ParserError: Invalid syntax at 0:8!'
+  ]
+end
+
 def build_repl_fiber(zil_context)
   Fiber.new {
     zil_context.globals[:REPL].call [], zil_context
