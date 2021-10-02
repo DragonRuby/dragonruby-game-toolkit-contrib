09debe159a218195deb032a75e7d035b6a0c0d63
Ferociousfeind <35383563+Ferociousfeind@users.noreply.github.com>
2021-10-01 20:12:23 -0400

    Minor Refactoring
    I feel that trying to use vertical alignment and math to replace/justify a magic number in a few small places would make things more confusing and obscure than just using defaults and the values that make it looks the prettiest...
    
    Also, I cannot figure out how to use those parts together to make it look good, unfortunately.

--- samples/99_zil_interpreter/app/main.rb
+++ samples/99_zil_interpreter/app/main.rb
@@ -5,28 +5,30 @@ require 'app/zil_repl.rb'
 require 'app/zil_context.rb'
 require 'app/eval.rb'
 
+BACK_COLOR = [0, 0, 0]
+TEXT_COLOR = [240, 255, 255]
+MAX_DISPLAYED_LINES = 33
+
 def tick(args)
   setup(args) if args.tick_count.zero?
   # Render history
   render_state(args)
-  
+
   # Collect input
   handle_input(args)
-  
+
   # Call send_input when pressing Enter
-  if args.inputs.keyboard.key_down.enter
-    send_input(args, args.state.input)
-  end
-  
+  send_input(args, args.state.input)
+
+
   $gtk.request_quit unless $interpreter.alive?
 end
 
 def setup(args)
-  args.state.text_color = [240, 255, 255]
-  args.state.bg_color = [0, 0, 0]
   args.state.input = ''
   args.state.text_history = []
-  args.state.current_line = 0
+  args.state.history_offset = 0
+
   context = build_zil_context(args)
   args.state.zil_context = context
   $interpreter = Fiber.new {
@@ -45,37 +47,37 @@ def handle_input(args)
   else
     if args.inputs.text[0]
       args.state.input << args.inputs.text[0]
-      args.state.current_line = 0
+      args.state.history_offset = 0
     end
   end
 
   # Scrolling
   if args.inputs.mouse.wheel
-    args.state.current_line += args.inputs.mouse.wheel.y
+    args.state.history_offset += args.inputs.mouse.wheel.y
 
     # Stop the player from scrolling past the beginning or end
-    max_possible_line = [0, args.state.text_history.length - 33].max
-    args.state.current_line = args.state.current_line.clamp(0, max_possible_line)
+    max_possible_offset = [0, args.state.text_history.length - MAX_DISPLAYED_LINES].max
+    args.state.history_offset = args.state.history_offset.clamp(0, max_possible_offset)
 
   # Jump to the present
   elsif args.inputs.keyboard.key_down.escape
-    args.state.current_line = 0
+    args.state.history_offset = 0
   end
 end
 
 # Called with the input after pressing enter
 def send_input(args, input)
-  return if input.empty?
+  if args.inputs.keyboard.key_down.enter && args.state.input != ''
+    $interpreter.resume input
 
-  $interpreter.resume input
+    args.state.text_history << " "
+    args.state.text_history << "> #{input}"
+    args.state.input = ''
+    args.state.history_offset = 0
 
-  args.state.text_history << " "
-  args.state.text_history << "> #{input}"
-  args.state.input = ''
-  args.state.current_line = 0
-
-  context = args.state.zil_context
-  process_outputs(args, context.outputs)
+    context = args.state.zil_context
+    process_outputs(args, context.outputs)
+  end
 end
 
 def process_outputs(args, outputs)
@@ -84,31 +86,31 @@ def process_outputs(args, outputs)
 end
 
 def render_state(args)
-  args.outputs.background_color = args.state.bg_color
-  
+  args.outputs.background_color = BACK_COLOR
+
   # Player input
   input_line = "> #{args.state.input}"
-  input_line << "_" if (args.tick_count / 32).round.mod_zero? 2 # Blinky underscore like all the old computers!
+  input_line << "_" if (args.tick_count / 32).round.mod_zero? 2 # Blinky underscore like in all the old computers!
   args.outputs.labels << {
-    x: 2, y: 22,
+    x: 8, y: 24,
     text: input_line,
-    r: args.state.text_color[0],
-    g: args.state.text_color[1],
-    b: args.state.text_color[2]
+    r: TEXT_COLOR[0],
+    g: TEXT_COLOR[1],
+    b: TEXT_COLOR[2]
   }
-  
+
   # Computer response
-  text_start = args.state.text_history.length - args.state.current_line
+  text_start = args.state.text_history.length - args.state.history_offset
   line = 0
-  while line < 34
+  while line <= MAX_DISPLAYED_LINES
     break if (text_start - line).negative?
     args.outputs.labels << {
-      x: 2, y: (line + 2) * 20,
+      x: 8, y: (line + 2) * 20,
       text: args.state.text_history[text_start - line],
-      r: args.state.text_color[0],
-      g: args.state.text_color[1],
-      b: args.state.text_color[2]
-    } 
+      r: TEXT_COLOR[0],
+      g: TEXT_COLOR[1],
+      b: TEXT_COLOR[2]
+    }
     line += 1
   end
 end
