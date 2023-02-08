5dc2e892befeffc9a7b02259c595e16c094da976
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-02-08 16:49:50 +0900

    Collect doc strings of all method docs

--- dragon/kernel_docs.rb
+++ dragon/kernel_docs.rb
@@ -66,6 +66,26 @@ S
 
     nil
   end
+
+  def export_api_docs!
+    entries = []
+    $docs_classes.each do |klass|
+      next if klass == GTK::ReadMe # Ignore text content of docs
+
+      DocsOrganizer.find_methods_with_docs(klass).each do |doc_method|
+        next if doc_method == :docs_class # Ignore class description
+        next if doc_method.to_s.start_with?('docs_api_summary') # Ignore API summary texts
+
+        entry = {
+          class: klass.name,
+          method: doc_method.to_s[5..],
+          raw_text: klass.send(doc_method)
+        }
+        puts "Exporting docs for #{entry[:class]}##{entry[:method]}"
+        entries << entry
+      end
+    end
+  end
 end
 
 module Kernel
